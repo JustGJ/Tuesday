204e2d79226850e3b94f4b8215455a9c0d8983db
Jeff <jf.gasp@gmail.com>
2022-10-06 09:55:37 +0200

    resolve conflit

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,33 @@ import types from './typeDefs';
 import resolvers from './resolvers';
 import { typeDefs as scalarsTypedefs, resolvers as scalarsResolvers } from 'graphql-scalars';
 import { decodedToken } from './decodedToken';
-import { prisma } from './prismaclient'
+import { prisma } from './prismaclient';
 
 const schema = makeExecutableSchema({
   typeDefs: [types, ...scalarsTypedefs],
   resolvers: { ...resolvers, ...scalarsResolvers },
 });
-const server = new ApolloServer({ schema,  csrfPrevention: true, context: async ({ req }) => {
-  // Get the user token from the headers.
-  console.log("ici")
-  const payload = decodedToken(req)
+const server = new ApolloServer({
+  schema,
+  csrfPrevention: true,
+  context: async ({ req }) => {
+    // Get the user token from the headers.
+    const payload = decodedToken(req);
 
-  let user; 
+    let user;
 
-  if(payload) {
-    user = await prisma.user.findUnique({
-      where: {
-        email: payload.email
-      }
-    });
-  } 
+    if (payload) {
+      user = await prisma.user.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+    }
 
-  console.log(user)
-  // Add the user to the context
-  return { user };
-  }
+    console.log(user);
+    // Add the user to the context
+    return { user };
+  },
 });
 
 server.listen({ port: 4004 }, () => {
